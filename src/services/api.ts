f687fa87f80ba2a9f4004937ae2eed96827b64ad
Arthur Morais <arthrmrs@gmail.com>
2024-09-11 12:52:49 -0300

    feat: Adicionado Service API - pacote axios adicionado ao projeto

---
+++ src/services/api.ts
@@ -0,0 +1,42 @@
+import axios from "axios";
+
+// Defina a URL base para o seu backend
+const API_BASE_URL = "https://gune1g5oyf.execute-api.us-east-1.amazonaws.com/Prod";
+
+const api = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// Função para listar dispositivos
+export const fetchDevices = async () => {
+  try {
+    const response = await api.get("/devices");
+    return response.data;
+  } catch (error) {
+    console.error("Erro ao buscar dispositivos", error);
+    throw error;
+  }
+};
+
+// Função para obter dados do dispositivo por ID
+export const fetchDeviceData = async (
+  deviceId: string,
+  startTime?: number,
+  endTime?: number
+) => {
+  try {
+    const response = await api.get(`/devices/${deviceId}/data`, {
+      params: {
+        start_time: startTime,
+        end_time: endTime,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error(`Erro ao buscar dados do dispositivo ${deviceId}`, error);
+    throw error;
+  }
+};
